4a9502cfd21624bcae81bb1c7195c914b79a9a6c
adminthw <154589317@qq.com>
2021-04-15 20:53:20 +0800

    完成切换城市页面

---
+++ pages/changeCity.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page-changeCity">
+    <el-row>
+      <el-col :span="24"><ISelect /></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24"><Hot /></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24"><Categroy /></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import ISelect from "../components/chanegCity/iselect";
+import Hot from "../components/chanegCity/hot";
+import Categroy from "../components/chanegCity/categroy";
+export default {
+  components: {
+    ISelect,
+    Hot,
+    Categroy,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
+
--- server/interface/geo.js
+++ server/interface/geo.js
@@ -2,6 +2,7 @@ import Router from "koa-router";
 import axios from "./utils/axios";
 import Menu from "../dbs/models/menus";
 import Province from "../dbs/models/provinces";
+import City from "../dbs/models/cities";
 let router = new Router({ prefix: "/geo" });
 router.get("/getPosition", async ctx => {
   let {
@@ -35,6 +36,61 @@ router.get("/province", async ctx => {
       return {
         id: item.id,
         name: item.value[0]
+      };
+    })
+  };
+});
+router.get("/province/:id", async ctx => {
+  let city = await City.findOne({ id: ctx.params.id });
+
+  ctx.body = {
+    code: 0,
+    city: city.value.map(item => {
+      return { province: item.province, id: item.id, name: item.name };
+    })
+  };
+});
+router.get("/hotCity", async ctx => {
+  let list = [
+    "北京市",
+    "上海市",
+    "广州市",
+    "深圳市",
+    "天津市",
+    "西安市",
+    "杭州市",
+    "南京市",
+    "武汉市",
+    "成都市"
+  ];
+  let result = await City.find();
+  let nList = [];
+  result.forEach(item => {
+    nList = nList.concat(
+      item.value.filter(k => list.includes(k.name) || list.includes(k.province))
+    );
+  });
+  ctx.body = {
+    code: 0,
+    hots: nList
+  };
+});
+router.get("/city", async ctx => {
+  let city = [];
+  let result = await City.find();
+  result.forEach(item => {
+    city = city.concat(item.value);
+  });
+  ctx.body = {
+    code: 0,
+    city: city.map(item => {
+      return {
+        province: item.province,
+        id: item.id,
+        name:
+          item.name === "市辖区" || item.name === "省直辖县级行政区划"
+            ? item.province
+            : item.name
       };
     })
   };
